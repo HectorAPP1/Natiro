576fbe465ea9f57fc0cd848d5930e23d41501a99
Hector Valdes <hector.agustin20@gmail.com>
2025-10-15 21:57:29 -0300

    fix: ajustar header para desktop y móviles - fixed/sticky, padding simétrico y botones alineados

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-13fdf7ef'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ghrjcps60no"
+    "revision": "0.ch6sme06reo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
